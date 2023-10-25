b367da136d0359c19e6db5ce3cf5b96f8e4bddb1
sherlockof96 <kavishdoshi28@gmail.com>
2023-10-25 02:29:15 -0700

    add padding top and bottom

--- src/components/StickNoteList.js
+++ src/components/StickNoteList.js
@@ -4,7 +4,7 @@ import StickyNote from "./StickyNote";
 const StickyNoteList = ({notes}) => {
     return (
         <>
-            <div className="grid grid-cols-4 md:grid-cols-2 sm:grid-cols-1 gap-4 mr-auto ml-auto max-w-8xl pl-2 pr-2">
+            <div className="grid grid-cols-4 md:grid-cols-2 sm:grid-cols-1 gap-4 mr-auto ml-auto max-w-8xl pl-2 pr-2 pt-2 pb-2">
                 {notes.map((note) => <StickyNote date={note.date} text={note.text} color={note.color} nodeid={note.id}/>)}
             </div>
         </>
