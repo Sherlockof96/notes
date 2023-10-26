476ad93bcbe190a3b282cc27cdbee5c5995e80bd
sherlockof96 <kavishdoshi28@gmail.com>
2023-10-26 02:08:24 -0700

    dark mode

--- src/components/AddStickyNote.js
+++ src/components/AddStickyNote.js
@@ -21,10 +21,10 @@ const AddStickyNote = ({color, hoverColor, date, handleAddNote}) => {
     return (
         <>
             <div className={`h-52 border-r-10 p-2 flex flex-col justify-between ${color}`}>
-                <textarea value={noteText} onChange={handleChange} className={`border-none resize-none outline-none  ${color}`} rows="8" columns="10" placeholder="Type to add note"></textarea>
+                <textarea value={noteText} onChange={handleChange} className={`border-none resize-none outline-none dark:text-dark ${color}`} rows="8" columns="10" placeholder="Type to add note"></textarea>
                 <div className="flex align-middle justify-between">
-                    <small>{charLimit - noteText.length} Remaining</small>
-                    <button onClick={handleSaveClick} className={`bg-gray-100 border-none rounded-xl ${hoverColor} p-1`}>Save</button>
+                    <small className="dark:text-dark">{charLimit - noteText.length} Remaining</small>
+                    <button onClick={handleSaveClick} className={`dark:text-dark bg-gray-100 border-none rounded-xl ${hoverColor} p-1`}>Save</button>
                 </div>
             </div>
         </>
